7961fe89b3ccc75b0b032b18f08733b841d25b25
Urdiale <87990700+GabrielUrdialeHubXP@users.noreply.github.com>
2021-12-22 16:26:00 -0300

    feat: last commit

--- Personal-Website/src/app/app.component.ts
+++ Personal-Website/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Personal-Website';
+  numero=1;
+  ngOnInit(): void {
+    const x=10;
+
+    console.log(x+x)
+  }
+  somar1(){
+    this.numero=this.numero+1
+  }
+  subtrair1(){
+    this.numero=this.numero-1
+  }
+
 }
